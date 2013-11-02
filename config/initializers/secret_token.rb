fafa9039a61957224a57f783ee501b2e0702db12
rmtsukuru <randolph.113@osu.edu>
2013-11-02 16:22:09 -0400

    Initial Rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackathonFoodstuffs::Application.config.secret_key_base = '85327edad216b7922e5fc3893639aed06b4a1c4b0d23c0ce37acf42c99e192c4de35a7a4d9e8c9a0943298db7b211d0d2fded063044b2b1864574fe19c41d2a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackathonFoodstuffs::Application.config.session_store :cookie_store, key: '_Hackathon-Foodstuffs_session'
